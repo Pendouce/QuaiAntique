8e546026ef63170aef7c24f22f71cc1c0f1dad80
Penda <pendapro@live.fr>
2025-08-14 07:16:43 +0000

    Ajout de la carte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html"),
+  new Route("/carte", "La carte", "/pages/carte.html"),
+  new Route("/test", "Les reservations", "/pages/test.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
