0a64bcb90133adb8689a56d2b07d321c529547fd
Penda <pendapro@live.fr>
2025-08-15 03:19:11 +0000

    Ajout du dossier auth et des pages signi, signup,reservation, account, editPassword

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,15 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html"),
   new Route("/carte", "La carte", "/pages/carte.html"),
-  new Route("/test", "Les reservations", "/pages/test.html"),
+  new Route("/reservation", "Les reservations", "/pages/reservation.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route(
+    "/editPassword",
+    "Changer de mot de passe",
+    "/pages/auth/editPassword.html"
+  ),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
